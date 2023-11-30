51c550e6fdcbe965d3dab0091b59ceba45687549
jonglee823 <jh2@kakao.com>
2023-11-30 10:44:31 +0900

    cookie를 이용한 인증

--- src/main/java/com/accountbook/controller/AuthController.java
+++ src/main/java/com/accountbook/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.accountbook.controller;
 
+import com.accountbook.domain.Session;
 import com.accountbook.domain.User;
 import com.accountbook.exception.InvalidLoginInfo;
 import com.accountbook.repository.UserRepository;
@@ -8,30 +9,53 @@ import com.accountbook.response.SessionResponse;
 import com.accountbook.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
-    //private final UserRepository userRepository;
     private final AuthService authService;
+    /*
+    설명 : id, password json 요청시 로그인 및 토큰 생성 및 반환
+    param: String Id, String Passwrod
+    return : SessionResponse(token)
+    */
+//    @PostMapping("/auth/login")
+//    public SessionResponse login(@RequestBody Login login){
+//        log.info(">>>>>>={}", login);
+//
+//        return authService.signin(login);
+//    }
 
+    /*
+    설명 : id, password json 요청시 로그인 및 토큰 생성 후 cookie 설정
+    param: String Id, String Passwrod
+    return : SessionResponse(token)
+    */
     @PostMapping("/auth/login")
-    public SessionResponse login(@RequestBody Login login){
-        //json ID/ PW
-        log.info(">>>>>>={}", login);
+    public ResponseEntity<?> login(@RequestBody Login login) {
+        SessionResponse sessionResponse = authService.signin(login);
 
-        //DB 조회
-//        User user = userRepository.findByEmailAndPassword(login.getEmail(), login.getPassword())
-//                .orElseThrow(()-> new InvalidLoginInfo());
+        ResponseCookie cookie = ResponseCookie.from("SESSION", sessionResponse.getToken())
+                .domain("localhost")
+                .httpOnly(true)
+                .path("/")
+                .sameSite("strict")
+                .maxAge(Duration.ofDays(30))
+                .build();
 
-        return authService.signin(login);
-        //TOKEN 응답
-    //return user;
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE,cookie.toString())
+                .build();
     }
 
 }
--- src/main/java/com/accountbook/resolver/AuthResolver.java
+++ src/main/java/com/accountbook/resolver/AuthResolver.java
@@ -16,6 +16,9 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 import javax.security.auth.message.AuthException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -30,17 +33,42 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String accessToken = webRequest.getHeader("Authorization");
+        //String accessToken = webRequest.getHeader("Authorization");
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
 
-        //Session null이면 401
-        if(accessToken == null || accessToken.isEmpty()){
-            throw new AuthException();
+        if(null == request){
+            log.error("HttpServletRequest is null ");
+            throw new Unauthorized();
+        }
+
+        Cookie[] cookies = request.getCookies();
+
+        for (Cookie cookie : cookies) {
+            log.info(">>>>>>>>> cookie.getValue() {} ", cookie.getValue().toString());
         }
 
 
-        Session session = sessionRepository.findByToken(accessToken)
-                .orElseThrow(Unauthorized::new);
+        if(cookies.length == 0){
+            log.error("cookies.length == 0 ");
+            throw new Unauthorized();
+        }
+        log.info(">>>>>> cookie.length = {}", cookies.length);
 
+        String token = cookies[0].getValue();
+
+        Session session = sessionRepository.findByToken(token)
+                .orElseThrow(Unauthorized::new);
         return new UserSession(session);
+
+
+//        //Session null이면 401
+//        if(accessToken == null || accessToken.isEmpty()){
+//            throw new AuthException();
+//        }
+//
+//
+//        Session session = sessionRepository.findByToken(accessToken)
+//                .orElseThrow(Unauthorized::new);
+
     }
 }
